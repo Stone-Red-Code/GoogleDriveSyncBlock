4277c4f2859ad792892cb090ce230ea357f6345b
StoneRed5 <56473591+StoneRed5@users.noreply.github.com>
2020-12-15 14:51:52 +0100

    Initial commit

---
+++ GoogleDriveSyncBlock/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace GoogleDriveSyncBlock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.MainTimer = new System.Windows.Forms.Timer(this.components);
+            this.SaveFilePathButton = new System.Windows.Forms.Button();
+            this.PathTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // MainTimer
+            // 
+            this.MainTimer.Interval = 1000;
+            this.MainTimer.Tick += new System.EventHandler(this.MainTimer_Tick);
+            // 
+            // SaveFilePathButton
+            // 
+            this.SaveFilePathButton.Location = new System.Drawing.Point(384, 58);
+            this.SaveFilePathButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.SaveFilePathButton.Name = "SaveFilePathButton";
+            this.SaveFilePathButton.Size = new System.Drawing.Size(45, 36);
+            this.SaveFilePathButton.TabIndex = 1;
+            this.SaveFilePathButton.Text = "...";
+            this.SaveFilePathButton.UseVisualStyleBackColor = true;
+            this.SaveFilePathButton.Click += new System.EventHandler(this.SaveFilePathButton_Click);
+            // 
+            // PathTextBox
+            // 
+            this.PathTextBox.Location = new System.Drawing.Point(18, 58);
+            this.PathTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.PathTextBox.Name = "PathTextBox";
+            this.PathTextBox.ReadOnly = true;
+            this.PathTextBox.Size = new System.Drawing.Size(355, 31);
+            this.PathTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Google Drive Path:";
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.AutoSize = true;
+            this.StatusLabel.Location = new System.Drawing.Point(18, 103);
+            this.StatusLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(315, 25);
+            this.StatusLabel.TabIndex = 3;
+            this.StatusLabel.Text = "Google Drive Sync is running:...";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 152);
+            this.Controls.Add(this.StatusLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SaveFilePathButton);
+            this.Controls.Add(this.PathTextBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "GoogleDriveSyncBlock";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer MainTimer;
+        private System.Windows.Forms.Button SaveFilePathButton;
+        private System.Windows.Forms.TextBox PathTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label StatusLabel;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ GoogleDriveSyncBlock/Form1.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoogleDriveSyncBlock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool GDrive_isRunning = false;
+        RegistryKey key = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\GoogleDriveBlock");
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            KillGDrive();
+
+            //Test if registry value exists
+            if (key.GetValue("Path") == null)
+            {
+                //Set Standart Path
+                key.SetValue("Path", @"C:\Program Files\Google\Drive\googledrivesync.exe");
+            }
+
+            PathTextBox.Text = (string)key.GetValue("Path");
+
+            MainTimer.Start();
+
+            notifyIcon1.Icon = this.Icon;
+            notifyIcon1.Visible = true;
+            notifyIcon1.Text = this.Text;
+        }
+
+        private void MainTimer_Tick(object sender, EventArgs e)
+        {
+            
+
+            StatusLabel.Text = "Google Drive Sync is running:" + GDrive_isRunning.ToString();
+
+
+            //Check if Visual Studio is running
+            Process[] pname = Process.GetProcessesByName("devenv");
+            if (pname.Length == 0)
+            {
+                if (GDrive_isRunning == false)
+                {
+                    GDrive_isRunning = true;
+                    StartGDrive();
+                }
+            }
+            else
+            {
+                GDrive_isRunning = false;
+                KillGDrive();
+            }
+        }
+
+        void KillGDrive()
+        {
+            try
+            {
+                foreach (Process proc in Process.GetProcessesByName("googledrivesync"))
+                {
+                    proc.Kill();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        void StartGDrive()
+        {
+            try
+            {
+                string filePath = @"C:\Program Files\Google\Drive\googledrivesync.exe";
+                Process.Start(filePath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void SaveFilePathButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "(*.exe)|*.exe";
+            ofd.ShowDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                PathTextBox.Text = ofd.FileName;
+                //Save Path in Registry
+                key.SetValue("Path", ofd.FileName);
+            }
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            ShowInTaskbar = true;
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized && ShowInTaskbar == true)
+            {
+                ShowInTaskbar = false;
+            }
+        }
+    }
+}
